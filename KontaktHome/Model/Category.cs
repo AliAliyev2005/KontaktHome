42a5f94caef75c926dbe2431ceae8901c1eb6b13
Xliy_g8zy@itstep.edu.az <Xliy_g8zy@itstep.edu.az>
2021-07-23 14:53:19 +0400

    Added Categories and style

--- KontaktHome/Model/Category.cs
+++ KontaktHome/Model/Category.cs
@@ -8,5 +8,6 @@ namespace KontaktHome.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- KontaktHome/ViewModel/MainViewModel.cs
+++ KontaktHome/ViewModel/MainViewModel.cs
@@ -19,9 +19,20 @@ namespace KontaktHome.ViewModel
         public MainViewModel()
         {
             Categories = new List<Category>();
-            Categories.Add(new Category() { Name = "Phones"});
-            Categories.Add(new Category() { Name = "Laptops" });
-            Categories.Add(new Category() { Name = "Accesories" });
+            Categories.Add(new Category() { Name = "Telefonlar və Planşetlər", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/telefon_planshet.png" });
+            Categories.Add(new Category() { Name = "Saatlar və qolbaqlar", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/saatlar_qolbaqlar.png" });
+            Categories.Add(new Category() { Name = "TV, audio video", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/TV_audio_video.png" });
+            Categories.Add(new Category() { Name = "Kompüter və aksesuarlar", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/komputer_ofis.png" });
+            Categories.Add(new Category() { Name = "Foto və Video", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/foto_video.png" });
+            Categories.Add(new Category() { Name = "Hobbi və Əyləncə", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/oyun_eylence.png" });
+            Categories.Add(new Category() { Name = "Məişət texnikası", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/meishet.png" });
+            Categories.Add(new Category() { Name = "Gözəllik və sağlamlıq", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/fen.png" });
+            Categories.Add(new Category() { Name = "Alətlər və Avtomobil məhsulları", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/elektrik-aletler_avtomobil.png" });
+            Categories.Add(new Category() { Name = "Ev və bağ", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/bag_ev.png" });
+            Categories.Add(new Category() { Name = "Mebel və Ev tekstili", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/mebel.png" });
+            Categories.Add(new Category() { Name = "Ofis avadanlıqları", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/mekteb.png" });
+            Categories.Add(new Category() { Name = "Uşaq aləmi", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/ushaq.png" });
+            Categories.Add(new Category() { Name = "Hədiyyə və Bonus kartları", ImageUrl = "https://kontakt.az/wp-content/uploads/2021/03/hediyye_karti.png" });
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
